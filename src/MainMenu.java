53da6b38a90c9ecefb08dffb4f767dcc249360fc
ramcj7742 <ramcj7742@AZRP-STD-1a82.wrdsb.ca>
2021-12-15 08:43:26 -0500

    Upload

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -1,3 +1,9 @@
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.util.Scanner;
+import javax.imageio.ImageIO;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,5 +15,13 @@
  * @author ramcj7742
  */
 public class MainMenu {
+    private String images;
     
+    MainMenu(){
+        try{
+            Scanner input = new Scanner(new File("AnimationImages.txt"));
+        }catch(Exception e){
+            
+        }
+    }
 }
