a3b7ca61cc01c40816134cb066b92c124e270e1f
pione <pione@192.168.2.151>
2021-12-16 22:30:29 -0500

    Added looking functionality and camera system

--- src/Player.java
+++ src/Player.java
@@ -1,3 +1,8 @@
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import javax.imageio.ImageIO;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,5 +14,42 @@
  * @author ramcj7742
  */
 public class Player {
+    public BufferedImage map;
+    public BufferedImage Cam1A;
+    public BufferedImage Cam1B;
+    public BufferedImage Cam1C;
+    public BufferedImage Cam2A;
+    public BufferedImage Cam2B;
+    public BufferedImage Cam3;
+    public BufferedImage Cam4A;
+    public BufferedImage Cam4B;
+    public BufferedImage Cam5;
+    public BufferedImage Cam7;
     
+    public BufferedImage left;
+    public BufferedImage right;
+    public BufferedImage butLeft;
+    public BufferedImage butRight;
+    
+    public Player(){
+        try{
+            map = ImageIO.read(new File("images//Cam_Map.PNG"));
+            Cam1A = ImageIO.read(new File("images//Cam1A.jpg"));
+            Cam1B = ImageIO.read(new File("images//Cam1B.jpg"));
+            Cam1C = ImageIO.read(new File("images//Cam1C.jpg"));
+            Cam2A = ImageIO.read(new File("images//Cam2A.PNG"));
+            Cam2B = ImageIO.read(new File("images//Cam2B.jpg"));
+            Cam3 = ImageIO.read(new File("images//Cam3.jpg"));
+            Cam4A = ImageIO.read(new File("images//Cam4A.jpg"));
+            Cam4B = ImageIO.read(new File("images//Cam4B.jpg"));
+            Cam5 = ImageIO.read(new File("images//Cam5.jpg"));
+            Cam7 = ImageIO.read(new File("images//Cam7.jpg"));
+            left = ImageIO.read(new File("images//officeLeft.jpg"));
+            right = ImageIO.read(new File("images//officeRight.jpg"));
+            butLeft = ImageIO.read(new File("images//leftButton.PNG"));
+            butRight = ImageIO.read(new File("images//rightButton.PNG"));
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
 }
